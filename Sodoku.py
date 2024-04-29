bf9a11f35c4675c829b31d9dd7c52c57540adb4f
christan52 <sza.krisz52@gmail.com>
2024-04-29 13:08:50 +0200

    valid fuggveny visszateresi ertek, helyesirasi hibak valid egyszerusitese

--- Sodoku.py
+++ Sodoku.py
@@ -1,5 +1,5 @@
 #<-------------------------------------------->
-# Alap mátrix(sodoku) megadása
+# Alap mátrix(sudoku) megadása
 # Validálás
 megoldandoMatrix= [[0,0,6,0,0,4],
                    [0,2,0,6,0,0],
@@ -27,13 +27,6 @@ def valid(matrix, sor, oszlop, szam):
     if sor + oszlop == len(matrix) - 1:
         for i in range(len(matrix)):
             if matrix[i][len(matrix) - 1 - i] == szam:
-                return False
-
-    # 3x2-es méretű blokk ellenörzése, mivel nem csak a sor és oszlopban szerepelhet 6 különböző szám
-    start_sor, start_oszlop = 2 * (sor // 2), 3 * (oszlop // 3)
-    for i in range(start_sor, start_sor + 2):
-        for j in range(start_oszlop, start_oszlop + 3):
-            if matrix[i][j] == szam:
                 return False
 
     return True
@@ -70,4 +63,4 @@ if sudoku_megold(megoldandoMatrix):
   print("A Sudoku helyes megoldása:")
   matrixKiir(megoldandoMatrix)
 else:
-  print("A sudokunak nincs megoldása:")
+  print("A Sudokunak nincs megoldása:")
